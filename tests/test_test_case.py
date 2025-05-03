1f844b43b0d0a8ee25087343fc67b5b0d87f5e41
Renato Santos <renatossnt@gmail.com>
2025-05-03 14:01:19 -0300

    4. Testando TestCase

--- tests/test_test_case.py
+++ tests/test_test_case.py
@@ -1,31 +1,82 @@
 from testing_framework.test_case import TestCase
 from testing_framework.test_result import TestResult
+from .test_stub import TestStub
+from .test_spy import TestSpy
+class TestCaseTest(TestCase):
 
-class MyTest(TestCase):
     def set_up(self):
-        print("set_up")
+        self.result = TestResult()
 
-    def tear_down(self):
-        print('tear_down')
+    def test_result_success_run(self):
+        stub = TestStub('test_success')
+        stub.run(self.result)
+        assert self.result.summary() == '1 run, 0 failed, 0 error'
+
+    def test_result_failure_run(self):
+        stub = TestStub('test_failure')
+        stub.run(self.result)
+        assert self.result.summary() == '1 run, 1 failed, 0 error'
+
+    def test_result_error_run(self):
+        stub = TestStub('test_error')
+        stub.run(self.result)
+        assert self.result.summary() == '1 run, 0 failed, 1 error'
+
+    def test_result_multiple_run(self):
+        stub = TestStub('test_success')
+        stub.run(self.result)
+        stub = TestStub('test_failure')
+        stub.run(self.result)
+        stub = TestStub('test_error')
+        stub.run(self.result)
+        assert self.result.summary() == '3 run, 1 failed, 1 error'
     
-    def test_a(self):
-        print('test_a')
+    def test_was_set_up(self):
+        spy = TestSpy('test_method')
+        spy.run(self.result)
+        assert spy.was_set_up
 
-    def test_b(self):
-        print('test_b')
+    def test_was_run(self):
+        spy = TestSpy('test_method')
+        spy.run(self.result)
+        assert spy.was_run
 
-    def test_c(self):
-        print('test_c')
+    def test_was_tear_down(self):
+        spy = TestSpy('test_method')
+        spy.run(self.result)
+        assert spy.was_tear_down
+
+    def test_template_method(self):
+        spy = TestSpy('test_method')
+        spy.run(self.result)
+        assert spy.log == "set_up test_method tear_down"
+
 
 result = TestResult()
 
-test = MyTest('test_a')
+test = TestCaseTest('test_result_success_run')
 test.run(result)
 
-test = MyTest('test_b')
+test = TestCaseTest('test_result_failure_run')
 test.run(result)
 
-test = MyTest('test_c')
+test = TestCaseTest('test_result_error_run')
+test.run(result)
+
+test = TestCaseTest('test_result_multiple_run')
+test.run(result)
+
+
+test = TestCaseTest('test_was_set_up')
+test.run(result)
+
+test = TestCaseTest('test_was_run')
+test.run(result)
+
+test = TestCaseTest('test_was_tear_down')
+test.run(result)
+
+test = TestCaseTest('test_template_method')
 test.run(result)
 
 print(result.summary())
