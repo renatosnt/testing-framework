66087533c406b72e3b9e63f94f41f7a61f27a108
Renato Santos <renatossnt@gmail.com>
2025-05-03 14:24:57 -0300

    5. Classe TestSuite

--- tests/test_test_case.py
+++ tests/test_test_case.py
@@ -52,31 +52,3 @@ class TestCaseTest(TestCase):
         assert spy.log == "set_up test_method tear_down"
 
 
-result = TestResult()
-
-test = TestCaseTest('test_result_success_run')
-test.run(result)
-
-test = TestCaseTest('test_result_failure_run')
-test.run(result)
-
-test = TestCaseTest('test_result_error_run')
-test.run(result)
-
-test = TestCaseTest('test_result_multiple_run')
-test.run(result)
-
-
-test = TestCaseTest('test_was_set_up')
-test.run(result)
-
-test = TestCaseTest('test_was_run')
-test.run(result)
-
-test = TestCaseTest('test_was_tear_down')
-test.run(result)
-
-test = TestCaseTest('test_template_method')
-test.run(result)
-
-print(result.summary())
