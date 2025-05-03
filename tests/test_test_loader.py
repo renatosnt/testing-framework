788bddbf131cfb5ad148c1124816fc5250dc0192
Renato Santos <renatossnt@gmail.com>
2025-05-03 16:57:08 -0300

    6. Classes TestLoader e TestRunner

---
+++ tests/test_test_loader.py
@@ -0,0 +1,44 @@
+from testing_framework.test_loader import TestLoader
+from .test_stub import TestStub
+from .test_spy import TestSpy
+from testing_framework.test_suite import TestSuite
+from testing_framework.test_case import TestCase
+from testing_framework.test_runner import TestRunner
+class TestLoaderTest(TestCase):
+
+    def test_create_suite(self):
+        loader = TestLoader()
+        suite = loader.make_suite(TestStub)
+        assert len(suite.tests) == 3
+
+    def test_create_suite_of_suites(self):
+        loader = TestLoader()
+        stub_suite = loader.make_suite(TestStub)
+        spy_suite = loader.make_suite(TestSpy)
+
+        suite = TestSuite()
+        suite.add_test(stub_suite)
+        suite.add_test(spy_suite)
+
+        assert len(suite.tests) == 2
+
+    def test_get_multiple_test_case_names(self):
+        loader = TestLoader()
+        names = loader.get_test_case_names(TestStub)
+        assert names == ['test_error', 'test_failure', 'test_success']
+
+    def test_get_no_test_case_names(self):
+
+        class Test(TestCase):
+            def foobar(self):
+                pass
+
+        loader = TestLoader()
+        names = loader.get_test_case_names(Test)
+        assert names == []
+
+loader = TestLoader()
+suite = loader.make_suite(TestLoaderTest)
+
+runner = TestRunner()
+runner.run(suite)
